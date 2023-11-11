ac909e9cee4e69666d4f4e14940d2d71d28aabac
Mustifix <131684539+Mustifix@users.noreply.github.com>
2023-11-11 13:26:40 +0300

    Update Seo.tsx

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -12,13 +12,13 @@ const Seo = () => {
 
             <meta name="description" content="he/him ・ Full-Stack Developer" />
 
-            <meta property='og:url' content='https://romanwashere.xyz' />
+            <meta property='og:url' content='https://mustifix.xyz' />
             <meta property='og:type' content='website'/>
             <meta property="og:title" content={`${config.github.username} | Full-Stack Developer`} />
             <meta name="og:description" content="he/him ・ Full-Stack Developer" />
 
             <meta property="og:image" content="/icon.jpg" />
-            <meta property="og:image:alt" content="romanwashere.xyz" />
+            <meta property="og:image:alt" content="mustifix.xyz" />
             <meta property="og:locale" content="en_GB" />
             <meta name="theme-color" content="#00afe8" />
         </Helmet>

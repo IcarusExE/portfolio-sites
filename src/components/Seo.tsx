623597b1bacd264ef0e33bea24d759cdc5755e7c
Mustifix <131684539+Mustifix@users.noreply.github.com>
2023-11-12 16:19:36 +0300

    Update Seo.tsx

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -12,13 +12,14 @@ const Seo = () => {
 
             <meta name="description" content="he/him ・ Full-Stack Developer" />
 
-            <meta property='og:url' content='https://mustifixwashere.vercel.app' />
+            <meta property='og:url' content='https://mustifixwashere.vercel.app/' />
             <meta property='og:type' content='website'/>
             <meta property="og:title" content={`${config.github.username} | Full-Stack Developer`} />
             <meta name="og:description" content="he/him ・ Full-Stack Developer" />
 
             <meta property="og:image" content="/logo.jpg" />
-            <meta property="og:image:alt" content="mustifixwashere.vercel.app"
+            <meta property="og:image:alt" content="mustifixwashere.vercel.app/" />
+            <meta property="og:locale" content="en_GB" />
             <meta name="theme-color" content="#00afe8" />
         </Helmet>
     );

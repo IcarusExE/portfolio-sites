030b13ecc3953b1803ded631e9f2f0a47a6ea7ff
Mustifix <131684539+Mustifix@users.noreply.github.com>
2023-11-12 16:18:47 +0300

    Update Seo.tsx

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,21 +5,20 @@ const Seo = () => {
     return (
         <Helmet>
             <title>Home | {config.github.username}</title>
-            <link rel="icon" type="image/x-icon" href="/icon.jpg" />
+            <link rel="icon" type="image/x-icon" href="/logo.jpg" />
 
             <meta name='robots' content='index, follow'/>
             <meta name='googlebot' content='index,follow'/>
 
             <meta name="description" content="he/him ・ Full-Stack Developer" />
 
-            <meta property='og:url' content='https://mustifix.xyz' />
+            <meta property='og:url' content='https://mustifixwashere.vercel.app' />
             <meta property='og:type' content='website'/>
             <meta property="og:title" content={`${config.github.username} | Full-Stack Developer`} />
             <meta name="og:description" content="he/him ・ Full-Stack Developer" />
 
-            <meta property="og:image" content="/icon.jpg" />
-            <meta property="og:image:alt" content="mustifix.xyz" />
-            <meta property="og:locale" content="en_GB" />
+            <meta property="og:image" content="/logo.jpg" />
+            <meta property="og:image:alt" content="mustifixwashere.vercel.app"
             <meta name="theme-color" content="#00afe8" />
         </Helmet>
     );
